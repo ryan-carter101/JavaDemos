99a733bfdccebe25de5c56c6f08cab4eb4d702a6
rCarter101 <ryan.carter413@gmail.com>
2021-05-22 18:34:31 -0400

    Create huffman.java
    Project to create a huffman compression algorithm. Unfinished.

---
+++ huffman.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+/*takes a txt file as a command line argument, and develops a huffman tree according to it.
+Huffman then creates a Gui to allow users to input different txt files, and ouputs compressed txt files.
+*/
+public class huffman{
+	public static void main(String[] args) {
+		if (args.length!=1){
+			System.out.println("Format is \"java huffman [filepath]\". Please try again.");
+			return;
+		}
+		File source = new File(args[0]);
+		try{
+			Scanner in = new Scanner(source);
+			in.close();
+		}
+		catch(FileNotFoundException e){
+			System.out.println("File "+args[0]+" does not exist!");
+		}
+	}
+}
